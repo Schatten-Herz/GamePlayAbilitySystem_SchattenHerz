9fcf279020b0dff327c341876e97ed2f0c552120
Hermirus <iszhangzhf@gmail.com>
2025-01-20 14:39:29 +0800

    first commit

---
+++ Source/FirstRPG/FirstRPG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FirstRPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstRPG, "FirstRPG" );
